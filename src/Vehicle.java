4807f3d9d295c90c9d25db541a13a0ed3587c530
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-25 09:31:32 -0500

    cosntructur created for vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,12 @@
-public class Vehicle {
+// abstract class representing a generic Vehicle.
+public abstract class Vehicle {
+    // Private attribute to store the license plate of the vehicle.
+    private String licensePlate;
+
+   //Constructor for the Vehicle class.
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+
 }
