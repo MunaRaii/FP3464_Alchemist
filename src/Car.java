bdac21088ac96318ddfe61a4c25e69f29eb38d39
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-25 09:37:39 -0500

    code added in car class to inherit from vehicle class and display info

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,11 @@
-public class Car {
+// Car.java
+public class Car extends Vehicle {
+    public Car(String licensePlate) {
+        super(licensePlate);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Car - License Plate: " + getLicensePlate());
+    }
 }
