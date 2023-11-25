155fd0706220bd38d4728227b5f61ac9f0eb28ae
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-25 09:45:46 -0500

    code added in bike class to inherit from vehicle class and display info

--- src/Bike.java
+++ src/Bike.java
@@ -1,2 +1,11 @@
-public class Bike {
+// Bike.java
+public class Bike extends Vehicle {
+    public Bike(String licensePlate) {
+        super(licensePlate);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Bike - License Plate: " + getLicensePlate());
+    }
 }
