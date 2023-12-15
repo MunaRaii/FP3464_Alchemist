3419b6308645e71f5642a2a7614c935f6990234c
munarai <munaraii1996@gmail.com>
2023-12-15 00:01:15 -0500

    initializes parking space with default EMPTY values

--- ParkingMap.java
+++ ParkingMap.java
@@ -1,2 +1,18 @@
 public class ParkingMap {
+
+    // Initializing ParkingMap with constants for EMPTY and OCCUPIED spaces
+    public static final int EMPTY = 0;
+    public static final int OCCUPIED = 1;
+    private final int[][] parkingSpace;
+
+    // Initializes a ParkingMap with default empty space
+    public ParkingMap(int rows, int cols){
+        this.parkingSpace = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                parkingSpace[i][j] = EMPTY; // default space empty
+            }
+        }
+    }
 }
