a1fa689a50f060b3b660e066ab33eaadaec7c3fe
rasiksilwal013 <rasiksilwal@gmail.com>
2023-12-15 18:55:47 -0500

    added welcomeuser method in ParkingMap method

--- src/ParkingMap.java
+++ src/ParkingMap.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class ParkingMap {
     public static final int EMPTY = 0;
     public static final int OCCUPIED = 1;
@@ -28,42 +26,6 @@ public class ParkingMap {
 
         } else {
             System.out.println("Please choose valid option.");
-        }
-    }
-
-    //method to display the parking map
-    public void displayParkingMap(){
-        for (int[] emptySlot : parkingSpace) {
-            for (int slots : emptySlot) {
-                System.out.println(
-                        slots == EMPTY ? "Empty" : "Occupied"
-                );
-            }
-        }
-    }
-
-    // method to check if a parking space is available
-    public boolean availableSpace(int row, int col){
-        return parkingSpace[row][col] == EMPTY;
-    }
-
-    // method to book parking space
-    public void bookSpace(int row, int col){
-        if(availableSpace(row, col)){
-            parkingSpace[row][col] = OCCUPIED;
-            System.out.println("Parking space booked.");
-        } else {
-            System.out.println("Selected parking space is already occupied.");
-        }
-    }
-
-    // method to free up a parking space
-    public void freeSpace(int row, int col){
-        if(parkingSpace[row][col] == OCCUPIED){
-            parkingSpace[row][col] = EMPTY;
-            System.out.println("Thank you for parking at our place. See you again!");
-        } else {
-            System.out.println("Selected parking space is already empty.");
         }
     }
 }
