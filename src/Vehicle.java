a54bc33156be30ead6c10c8eec560af1db7e21dc
rasiksilwal013 <rasiksilwal@gmail.com>
2023-12-15 18:35:33 -0500

    Created the Vehicle enum with two constants

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,18 +1,12 @@
-// abstract class representing a generic Vehicle.
-public abstract class Vehicle {
-    // Private attribute to store the license plate of the vehicle.
-    private String licensePlate;
+public enum Vehicle {
+    CAR(10), BIKE(7.5);
 
-   //Constructor for the Vehicle class.
-    public Vehicle(String licensePlate) {
-        this.licensePlate = licensePlate;
+    private final double price;
+
+    Vehicle(double price){
+        this.price = price;
     }
-    //Getter method to retrieve the license plate of the vehicle.
-    public String getLicensePlate() {
-        return licensePlate;
+    public double getPrice() {
+        return price;
     }
-
-    //display information used by subclass to display the licence plate number
-    public abstract void displayInfo();
-
 }
