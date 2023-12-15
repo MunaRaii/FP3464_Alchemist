cefb0709f28f01b928d295b5354a7f8426312091
munarai <munaraii1996@gmail.com>
2023-12-15 00:06:50 -0500

    welcomeUsers method added

--- ParkingMap.java
+++ ParkingMap.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ParkingMap {
 
     // Initializing ParkingMap with constants for EMPTY and OCCUPIED spaces
@@ -13,6 +15,23 @@ public class ParkingMap {
             for (int j = 0; j < cols; j++) {
                 parkingSpace[i][j] = EMPTY; // default space empty
             }
+        }
+    }
+
+    // Display Welcome message and prompts users to choose between parking a car or bike"
+    public void welcomeUsers()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to our parking app, please choose the vehicle type you want to park: " +
+                "\n1)Car\n2)Bike");
+        int option = scan.nextInt();
+        if (option == 1) {
+
+        } else if (option == 2) {
+
+        } else {
+            System.out.println("Please choose valid option.");
         }
     }
 }
